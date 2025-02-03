03a32ca2266784fc5bd0599270b2fb6e2be0a900
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-03 19:49:02 +0100

    refactor(loading-spinner) change style for loadin-spinner

--- src/util/loading-spinner.tsx
+++ src/util/loading-spinner.tsx
@@ -1,7 +1,9 @@
+import { Loader } from "lucide-react";
+
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center h-56">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
+    <div className="flex justify-center items-center ">
+      <Loader className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-primary" />
     </div>
   );
 };
