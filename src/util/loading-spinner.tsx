ef57c9d568f5dbaaa87a68c776bc8e94f30f0817
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-07 16:20:44 +0100

    refactor(loading-spinner) change UI loading-spinner

--- src/util/loading-spinner.tsx
+++ src/util/loading-spinner.tsx
@@ -1,9 +1,9 @@
-import { LoaderIcon } from "lucide-react";
+import { LoaderPinwheel } from "lucide-react";
 
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center ">
-      <LoaderIcon className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-primary" />
+      <LoaderPinwheel className="animate-spin rounded-full h-16 w-16 border-t-1 border-b-1 border-primary mb-56" />
     </div>
   );
 };
