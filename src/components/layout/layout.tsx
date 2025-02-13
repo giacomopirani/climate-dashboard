9de4cae71148ad27e6b11cedc7d6392ca76d9e96
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-13 12:45:26 +0100

    refactor(logo) for light & dark mode

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,8 @@
 import { Home } from "lucide-react";
 import type { ReactNode } from "react";
 import { Link } from "react-router-dom";
+import logoGlobeDark from "../../assets/globe-track-darkblue.png";
+import logoGlobeLight from "../../assets/globe-track-light.png";
 import Sidebar from "../layout/sidebar";
 import ThemeToggle from "../ui/theme-toggle";
 import Footer from "./footer";
@@ -18,10 +20,26 @@ export default function Layout({ children }: LayoutProps) {
       </div>
       <div className="flex flex-col flex-grow w-full">
         <header className="bg-secondary-foreground fixed top-0 left-0 right-0 flex items-center p-2 pl-4 z-10">
+          <Link to="/" className="flex items-center space-x-2">
+            <img
+              src={logoGlobeLight}
+              alt="Logo App"
+              className="h-14 dark:hidden"
+            />
+            <img
+              src={logoGlobeDark}
+              alt="Logo App"
+              className="h-14 hidden dark:block"
+            />
+            <span className="font-extralight text-white dark:text-slate-950">
+              {" "}
+              Global Warming Dashboard
+            </span>
+          </Link>
           <div className="flex-grow" />
           <div className="flex items-center space-x-4">
             <Link to="/">
-              <Home className="h-10 w-10 cursor-pointer text-white dark:text-blue-950 hover:text-teal-700 dark:hover:text-teal-700" />
+              <Home className="h-8 w-8 cursor-pointer text-white dark:text-blue-950 hover:text-teal-700 dark:hover:text-teal-700" />
             </Link>
             <ThemeToggle />
           </div>
