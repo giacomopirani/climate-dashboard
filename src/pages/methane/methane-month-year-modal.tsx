cc65df75593c7d6c6ae550f432a4b8d54d24089e
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-14 15:04:16 +0100

    refactor(methane-modal) add dark mode style

--- src/pages/methane/methane-month-year-modal.tsx
+++ src/pages/methane/methane-month-year-modal.tsx
@@ -16,9 +16,11 @@ export function MethaneMonthYearModal({
 }: MethaneMonthYearModalProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white w-full max-w-md md:rounded-lg p-4 flex flex-col">
+      <div className="bg-white w-full max-w-md md:rounded-lg p-4 flex flex-col dark:bg-cyan-950">
         <header className="flex justify-between items-center px-4 py-3 border-b">
-          <h2 className="text-xl font-bold text-black">Select Month & Year</h2>
+          <h2 className="text-xl font-bold text-black dark:text-white">
+            Select Month & Year
+          </h2>
           <button
             onClick={() => setSelectedMonth(defaultMonth)}
             className="text-red-500 hover:text-red-700"
